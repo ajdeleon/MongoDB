f3154a841da200a53677dae0d293d309ac87963b
AJ de Leon <ajdeleon1064@gmail.com>
2017-04-04 22:25:10 -0400

    add test to add subdoc to existing record

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -16,4 +16,23 @@ describe("Subdocuments", () => {
         done()
       })
   })
+
+  it("can add subdocuments to an existing record", (done) => {
+    const joe = new User({
+      name: "Joe",
+      posts: []
+    })
+    joe.save() //save joe
+      .then(() => User.findOne({ name: "Joe"})) //fetch jose
+      .then((user) => {
+        user.posts.push({ title: "New Post"}) //common js operation
+        return user.save() //persist to database. must return to make promise because => {}
+      })
+      .then(() => User.findOne({ name: "Joe"})) //fetch joe again
+      .then((user) => {
+        assert(user.posts[0].title === "New Post")
+        done()
+      })
+  })
+
 })
